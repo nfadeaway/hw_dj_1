a4650f9200920b84f7632b8878efe27a9d84736f
Evgeniy Degtyar <farappl@gmail.com>
2023-02-02 09:01:55 +0500

    hw_dj_3.3 fix validate function

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -42,7 +42,10 @@ class AdvertisementSerializer(serializers.ModelSerializer):
         adv_status_open = Advertisement.objects.filter(creator=self.context["request"].user.pk).filter(
             status="OPEN").count()
         if adv_status_open >= 10:
-            raise serializers.ValidationError('У вас уже 10 открытых объявлений!')
+            if self.context["view"].action == "create":
+                raise serializers.ValidationError('У вас уже 10 открытых объявлений! Вы не можете создать новое!')
+            if self.context["view"].action == "partial_update" and data["status"] == "OPEN":
+                raise serializers.ValidationError('У вас уже 10 открытых объявлений! Вы не можете открыть больше!')
 
         return data
 
